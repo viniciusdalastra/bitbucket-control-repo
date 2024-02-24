3e4af1ca504289c34bb91f526fb5419498a8adc7
dalastraa <vinicius.dalastra@omnismart.io>
2024-02-24 17:00:47 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ allowedHeaders: '', origin: '*' });
 
   const config = new DocumentBuilder()
     .setTitle('Bitbucket Sync Service')
